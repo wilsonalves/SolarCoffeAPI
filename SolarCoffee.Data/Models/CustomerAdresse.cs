eae7a07fff0229228c0f200c03d7fafdd8d3f80e
Wilson Alves <wilson_9567@hotmail.com>
2020-08-13 00:46:04 -0300

    migrations

---
+++ SolarCoffee.Data/Models/CustomerAdresse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SolarCoffee.Data.Models
+{
+    public class CustomerAdresse
+    {
+
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdateOn { get; set; }
+       
+        [MaxLength(100)]
+        public string AddressLine1 { get; set; }
+        [MaxLength(100)]
+        public string AddressLine2 { get; set; }
+        [MaxLength(100)]
+        public string City { get; set; }
+        [MaxLength(2)]
+        public string State { get; set; }
+        [MaxLength(12)]
+        public string PostalCode { get; set; }
+        [MaxLength(60)]
+        public string Country { get; set; }
+    }
+}
